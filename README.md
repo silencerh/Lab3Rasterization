caf86e35365df0113213f0c221ead8c2fbe5d5fb
renhao.zju@gmail.com <renhao.zju@gmail.com>
2015-04-23 16:02:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Lab3 Rasterization
+
+This is the lab assignment for DH2323
---
+++ skeleton.cpp
@@ -0,0 +1,184 @@
+﻿#include <iostream>
+#include <glm/glm.hpp>
+#include <SDL.h>
+#include "SDLauxiliary.h"
+#include "TestModel.h"
+
+using namespace std;
+using glm::vec3;
+using glm::ivec2;
+using glm::mat3;
+using glm::vec2;
+
+// ----------------------------------------------------------------------------
+// GLOBAL VARIABLES
+
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
+SDL_Surface* screen;
+int t;
+vector<Triangle> triangles;
+vec3 cameraPos( 0, 0, -3.001 );
+// ----------------------------------------------------------------------------
+// FUNCTIONS
+
+void Update();
+void Draw();
+void VertexShader(const vec3& v, ivec2& p);
+void DrawLineSDL(SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color);
+void DrawPolygonEdges(const vector<vec3>& vertices);
+
+int main( int argc, char* argv[] )
+{
+	LoadTestModel( triangles );
+	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
+	t = SDL_GetTicks();	// Set start value for timer.
+
+	while( NoQuitMessageSDL() )
+	{
+		Update();
+		Draw();
+	}
+
+	SDL_SaveBMP( screen, "screenshot.bmp" );
+	return 0;
+}
+
+void Update()
+{
+	// Compute frame time:
+	int t2 = SDL_GetTicks();
+	float dt = float(t2-t);
+	t = t2;
+	cout << "Render time: " << dt << " ms." << endl;
+
+	Uint8* keystate = SDL_GetKeyState(0);
+
+	if (keystate[SDLK_UP])
+		;
+
+	if( keystate[SDLK_DOWN] )
+		;
+
+	if( keystate[SDLK_RIGHT] )
+		;
+
+	if( keystate[SDLK_LEFT] )
+		;
+
+	if( keystate[SDLK_RSHIFT] )
+		;
+
+	if( keystate[SDLK_RCTRL] )
+		;
+
+	if( keystate[SDLK_w] )
+		;
+
+	if( keystate[SDLK_s] )
+		;
+
+	if( keystate[SDLK_d] )
+		;
+
+	if( keystate[SDLK_a] )
+		;
+
+	if( keystate[SDLK_e] )
+		;
+
+	if( keystate[SDLK_q] )
+		;
+}
+
+void Draw()
+{
+	SDL_FillRect( screen, 0, 0 );
+
+	if( SDL_MUSTLOCK(screen) )
+		SDL_LockSurface(screen);
+	
+	for( int i=0; i<triangles.size(); ++i )
+	{
+		vector<vec3> vertices(3);
+
+		vertices[0] = triangles[i].v0;
+		vertices[1] = triangles[i].v1;
+		vertices[2] = triangles[i].v2;
+		vector<ivec2> projPos(3);
+		vec3 color(1, 1, 1);
+		DrawPolygonEdges(vertices);
+		/*for (int v = 0; v < 3; ++v)
+		{
+			VertexShader(vertices[v],projPos[v]);
+			PutPixelSDL(screen,projPos[v].x,projPos[v].y,color);
+		}*/
+		//DrawLineSDL(screen, projPos[0],projPos[1],color);
+		//DrawLineSDL(screen, projPos[1], projPos[2], color);
+		//DrawLineSDL(screen, projPos[0], projPos[2], color);
+
+
+	}
+
+	
+	
+	if ( SDL_MUSTLOCK(screen) )
+		SDL_UnlockSurface(screen);
+
+	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+}
+
+void VertexShader(const vec3& v, ivec2& p)
+{
+
+	vec3 v_trans = v - cameraPos;
+	float f = SCREEN_WIDTH / 2;
+	p.x = f*v_trans.x/v_trans.z+SCREEN_WIDTH/2;
+	p.y = f*v_trans.y/v_trans.z+SCREEN_HEIGHT/2;
+
+}
+
+void Interpolate(ivec2 a, ivec2 b, vector<ivec2>& result)
+{
+	int N = result.size();
+	vec2 step = vec2(b - a) / float(fmax(N - 1, 1));
+	vec2 current(a);
+	for (int i = 0; i<N; ++i)
+	{
+		result[i] = current;
+		current += step;
+	}
+}
+
+void DrawLineSDL(SDL_Surface* surface, ivec2 a, ivec2 b, vec3 color)
+{
+	ivec2 delta = glm::abs(a - b);
+	int pixels = glm::max(delta.x, delta.y) + 1;
+	vector<ivec2> line(pixels);
+	Interpolate(a,b,line);
+	for (int j = 0; j < line.size(); ++j)
+	{
+		PutPixelSDL(screen, line[j].x, line[j].y,color);
+	}
+}
+
+void DrawPolygonEdges(const vector<vec3>& vertices)
+{
+	int V = vertices.size();
+	// Transform each vertex from 3D world position to 2D image position: 
+	vector<ivec2> projectedVertices(V);
+	for (int i = 0; i<V; ++i)
+	{
+		VertexShader(vertices[i], projectedVertices[i]);
+	}
+
+
+	// Loop over all vertices and draw the edge from it to the next vertex: 
+	for (int i = 0; i<V; ++i)
+	{
+		int j = (i + 1) % V; // The next vertex 
+		vec3 color(1, 1, 1);
+		DrawLineSDL(screen, projectedVertices[i], projectedVertices[j],
+			color);
+	}
+}
